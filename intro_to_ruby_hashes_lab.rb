964e1905e122c3c249dd88784c3ab4d1b366258c
Changmin Lim <chang.lim92@gmail.com>
2018-05-29 04:24:38 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,7 +33,7 @@ def monopoly_with_third_tier
 		  :two_piece_owned => "50",
 		  :three_pieces_owned => "100",
 		  :four_pieces_owned => "200",
-		}
+		},
 		:names => {
 		  :rading_railroad => {},
 		  :pennsylvania_railroad => {},
